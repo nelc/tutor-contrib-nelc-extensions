4928bf5007aa2b2786ac4b056977a49c05a85faa
Johan Castiblanco <johan.castiblanco@edunext.co>
2024-05-06 20:38:46 -0500

    refactor: rename var for settings

--- tutornelc_extensions/plugin.py
+++ tutornelc_extensions/plugin.py
@@ -24,7 +24,7 @@ hooks.Filters.CONFIG_DEFAULTS.add_items(
         ("ATLAS_OPTIONS", ""),
         ("NELC_EXTENSIONS_TRANSLATION_OVERRIDES_PATH", "/translation-overrides/conf/locale"),
         ("NELC_EXTENSIONS_TRANSLATION_OVERRIDES", True),
-        ("NELC_EXTENSIONS_TRANSLATION_FORCE_OVERRIDES", False),
+        ("NELC_EXTENSIONS_TRANSLATION_OVERRIDES_PRIORITY", False),
     ]
 )
 
