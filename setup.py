b84c698ce10cb5fdb7586417408500f01241196d
Johan Castiblanco <johan.castiblanco@edunext.co>
2025-11-28 16:06:57 -0500

    feat: bump version 20.0.0

--- setup.py
+++ setup.py
@@ -42,10 +42,10 @@ setup(
     packages=find_packages(exclude=["tests*"]),
     include_package_data=True,
     python_requires=">=3.7",
-    install_requires=["tutor>=18.0.0,<19.0.0", "importlib-resources>=6.1.1"],
+    install_requires=["tutor>=20.0.0,<21.0.0", "importlib-resources>=6.1.1"],
     extras_require={
         "dev": [
-            "tutor[dev]>=18.0.0,<19.0.0",
+            "tutor[dev]>=20.0.0,<21.0.0",
         ]
     },
     entry_points={
